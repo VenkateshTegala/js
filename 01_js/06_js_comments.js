f48da3c7104030abdc4a57a3156670bba2fe5180
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-04 19:52:16 +0530

    Added new file

---
+++ 01_js/06_js_comments.js
@@ -0,0 +1,18 @@
+//It's a single line comment
+
+/*
+    It is a 
+    multiline 
+    comment
+ */
+
+//using comments to prevent execution
+/*
+    function inner_html_function() {
+        document.getElementById('1').innerHTML="<button>Hello world!</button>";
+    }
+
+    function inner_text_function() {
+        document.getElementById('2').innerText="Hello it is text";
+    }
+ */
