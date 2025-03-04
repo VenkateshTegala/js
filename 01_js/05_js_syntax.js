bbe50389464316da9aac78b35e07e6c6a3500bca
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-04 19:52:06 +0530

    Added new file

---
+++ 01_js/05_js_syntax.js
@@ -0,0 +1,17 @@
+//how to create a variable:
+var x;
+let y;
+//using a variable
+x = 5;
+y = 6;
+let z = x + y;
+
+//strings can be wriiten in double or single quotes
+a = "John Doe";
+b = 'John Doe';
+
+//operations
+(5 + 6) * 10
+
+let value = 5; //It will be executed
+// x = 6; It will not be executed
