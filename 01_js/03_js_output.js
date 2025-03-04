1f71ffb76178614ab56c52af465cda1b92188ec5
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-04 19:13:25 +0530

    Added new file

---
+++ 01_js/03_js_output.js
@@ -0,0 +1,15 @@
+function inner_html_function() {
+    document.getElementById('1').innerHTML="<button>Hello world!</button>";
+}
+
+function inner_text_function() {
+    document.getElementById('2').innerText="Hello it is text";
+}
+
+function document_write_function() {
+    document.write(5 + 3);
+}
+
+function window_alert_function() {
+    window.alert(5 + 3);
+}
