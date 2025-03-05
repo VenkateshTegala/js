2447fa035ed2efe4b49b35aac4d681b3177062a6
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-05 23:33:41 +0530

    Added new file

---
+++ 01_js/07_js_variables.js
@@ -0,0 +1,29 @@
+//automatically
+x = 5;
+y = 6;
+z = x + y;
+
+//using var
+var x = 5;
+var y = 6;
+var z = x + y;
+
+//using let
+let x = 5;
+let y = 6;
+let z = x + y;
+
+//using const
+const x = 5;
+const y = 6;
+const z = x + y;
+
+//example
+const pi = 3.14;
+let person = "John Doe";
+
+//declaring and initializing variable
+let carname;
+carname = "Volvo";
+
+
